2b3d3325e74bb1de755b7af5c4d7d6fc5e6aa9b0
spitfire-hash <carlsonmu@gmail.com>
2020-08-24 18:18:43 +0400

    some commit

--- YPA_AZE/settings.py
+++ YPA_AZE/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('YPA_AZE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ("False" == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'https://ypaphysicsaze.herokuapp.com/']
 
